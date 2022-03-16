6fe1dd67b98c2ef7f49b103a57cc82eec2c89384
fazrulreza <fazrul89@gmail.com>
2022-03-16 23:58:11 +0800

    add english for msic

--- table/msic.sql
+++ table/msic.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `msic` (
 	`MSIC` VARCHAR(10) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_0900_ai_ci',
 	`description_malay_detail` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`description_english_detail` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
 	`section` VARCHAR(10) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
 	`division` VARCHAR(10) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
 	`group` VARCHAR(10) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`Class` VARCHAR(10) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`class` VARCHAR(10) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
 	`description_english_section` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
 	`description_malay_section` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
 	`description_english_division` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
